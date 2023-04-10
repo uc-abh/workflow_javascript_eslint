f226b9565569a997c6c187c9339b3ad73f4c0387
Abhishek Pandey <abhishek.pandey@ucertify.com>
2023-04-10 12:50:19 +0530

    kjg

--- abhipole.js
+++ abhipole.js
@@ -2,7 +2,7 @@ const num1 = 5;
 const num2 = 3;
 
 // add two numbers
-const sum = num1 + num2;
+cons sum = num1 + num2;
 
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
